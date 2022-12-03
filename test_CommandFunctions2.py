f80d6f0b69968b94ce2aeea8098d2ac75378dffe
benjo121ben <benjamin.wenger00@gmail.com>
2022-12-03 03:26:16 +0100

    initial setup, a few tests for loading savefiles

---
+++ test_CommandFunctions2.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from src.ext.Campaign import save_file_management as save_manager, CommandFunctions as commands
+
+from test_const_vars import unit_test_save_file_name
+
+
+class TestCommandFunctions(TestCase):
+    def setUp(self) -> None:
+        if save_manager.check_savefile_existence(unit_test_save_file_name):
+            save_manager.remove(unit_test_save_file_name)
+            print("deleted existing unit test savefile")
+        save_manager.load(unit_test_save_file_name)
+
+    def tearDown(self) -> None:
+        save_manager.remove(unit_test_save_file_name)
+
+    def test_log(self):
+        print(commands.add_char("test", "test", 20))
+        print(commands.log())
+        assert save_manager.check_savefile_existence(unit_test_save_file_name)
+
